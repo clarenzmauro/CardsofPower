45519b2d3a3c98b758f516bc36ecd807112b91cc
MannLester <127029268+MannLester@users.noreply.github.com>
2024-11-13 08:11:00 -0800

    November 14 Update: Inventory Page

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Link, useParams } from 'react-router-dom';
-import aiImage from '../assets/images/ai.png';
+import accountImage from '../assets/images/account.png';
 import battleImage from '../assets/images/battle.png';
 import shopImage from '../assets/images/shop.png';
 import inventoryImage from '../assets/images/inventory.png';
@@ -17,9 +17,9 @@ function HomePage() {
       <div className="overlay"></div>
 
       <div className='banners'>
-        <Link to={`/${userDocId}/battlefield`}><img src={aiImage} alt="" /></Link>
+        <Link to={`/${userDocId}/workshop`}><img src={workshopImage} alt="" /></Link>
         <Link to={`/${userDocId}/battlefield`}><img src={battleImage} alt="" /></Link>
-        <Link to="/shop"><img src={shopImage} alt="" /></Link>
+        <Link to={`/${userDocId}/shop`}><img src={shopImage} alt="" /></Link>
       </div>
 
       <div className='links text-white text-xl'>
@@ -38,16 +38,16 @@ function HomePage() {
         </div>
 
         <div className='sm:mx-6 lg:mx-12'>
-          <Link to={`/${userDocId}/workshop`}>
-            <img src={workshopImage} alt="" />
-            <p>Workshop</p>
+          <Link to={`/${userDocId}/friends`}>
+            <img src={friendsImage} alt="" />
+            <p>Friends</p>
           </Link>
         </div>
 
         <div className='sm:mx-6 lg:mx-12'>
-          <Link to={`/${userDocId}/friends`}>
-            <img src={friendsImage} alt="" />
-            <p>Friends</p>
+          <Link to={`/${userDocId}/account`}>
+            <img src={accountImage} alt="" />
+            <p>Account</p>
           </Link>
         </div>
       </div>
