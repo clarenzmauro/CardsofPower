f9e1b20f0488cc7b774e1639d8185f60fc018f10
MannLester <127029268+MannLester@users.noreply.github.com>
2024-11-18 09:55:46 -0800

    Merged With Jett. Done with Shop, Listing, and Trades

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,8 @@ import InventoryPage from "./components/InventoryPage.jsx";
 import DictionaryPage from "./components/DictionaryPage.jsx";
 import ListingPage from "./components/ListingPage.jsx";
 import ShopPage from "./components/ShopPage.jsx";
+import TradePageContext from "./components/TradePageContext.jsx";
+import TradePage from "./components/TradePage.jsx";
 import { CardsContext, CardsProvider } from "./components/Battlefield/CardsContext.jsx";
 import './index.css';
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
@@ -23,6 +25,7 @@ function App() {
         <Route path="/:userDocId/dictionary" element={<DictionaryPage />}/>
         <Route path="/:userDocId/shop/listing" element={<ListingPage />}/>
         <Route path="/:userDocId/shop" element={<ShopPage />}/>
+        <Route path="/:userDocId/shop/trades" element={<TradePage />}/> 
         {/* Only use for testing implementation */}
         {/* <Route path="/" element={<Battlefield />}/> */}
       </Routes>
