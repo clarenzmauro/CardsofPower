9dd52f6703c00b32bda5f7d36ee787251752104e
jettmanalo <164733601+jettmanalo@users.noreply.github.com>
2024-11-19 00:05:56 +0800

    Update DictionaryPage.jsx

--- src/components/DictionaryPage.jsx
+++ src/components/DictionaryPage.jsx
@@ -3,7 +3,10 @@ import { useParams } from "react-router-dom";
 import { firestore, storage } from "./firebaseConfig";
 import { collection, getDocs, doc, getDoc } from "firebase/firestore";
 import { ref as storageRef, getDownloadURL } from "firebase/storage";
-import './InventoryPage.css';
+
+import inventoryBg from "../assets/backgrounds/inventory.jpg";
+import gold from "../assets/images/gold.png";
+import "./InventoryPage.css";
 
 function DictionaryPage() {
   const { userDocId } = useParams();
@@ -16,6 +19,52 @@ function DictionaryPage() {
   const [cardClass, setCardClass] = useState(""); // For "Card Class" dropdown
   const [cardCharacter, setCardCharacter] = useState(""); // For "Card Character" dropdown
   const [cardLevel, setCardLevel] = useState(""); // For "Card Level" number input
+  const [selectedCard, setSelectedCard] = useState(null); // Track the selected card
+  const [goldCount, setGoldCount] = useState(0);
+
+  const winRate = selectedCard
+    ? // Ensure both cardWin and cardMatch have valid values for calculation
+      (selectedCard.cardWin?.local || 0) && (selectedCard.cardMatch?.local || 0)
+      ? (
+          ((selectedCard.cardWin.local || 0) /
+            (selectedCard.cardMatch.local || 0)) *
+          100
+        ).toFixed(2) + "%"
+      : "0%" // Treat 0 as a valid win rate instead of N/A
+    : "N/A";
+
+  const marketValue = selectedCard?.marketValue || 0; // Default to 0 if no value
+  const boughtFor = selectedCard?.boughtFor || 0; // Default to 0 if no value
+  const roi = marketValue - boughtFor; // ROI calculation
+
+  // Determine the formatted ROI with color
+  const formattedROI = (() => {
+    if (roi > 0) {
+      return <span className="text-green-500">+{roi}</span>; // Positive ROI in green
+    } else if (roi < 0) {
+      return <span className="text-red-600">-{Math.abs(roi)}</span>; // Negative ROI in red
+    } else {
+      return <span>{roi}</span>; // ROI of 0 in default color
+    }
+  })();
+
+  useEffect(() => {
+    async function fetchUserData() {
+      try {
+        const userDocRef = doc(firestore, "users", userDocId);
+        const userDoc = await getDoc(userDocRef);
+
+        if (userDoc.exists()) {
+          const userData = userDoc.data();
+          setGoldCount(userData.goldCount || 0);
+        }
+      } catch (error) {
+        console.error("Error fetching user data:", error);
+      }
+    }
+
+    fetchUserData();
+  }, [userDocId]);
 
   useEffect(() => {
     const fetchInventoryAndCards = async () => {
@@ -35,7 +84,9 @@ function DictionaryPage() {
 
           // Ensure the card has a 'cardName' property
           if (!cardData.cardName) {
-            console.warn(`Card with ID ${card.id} is missing a 'cardName' property.`);
+            console.warn(
+              `Card with ID ${card.id} is missing a 'cardName' property.`
+            );
             cardData.cardName = "Unknown Card"; // Provide a default value
           }
 
@@ -60,129 +111,209 @@ function DictionaryPage() {
   }, [userDocId]);
 
   // Filter the cards based on the search query and other dropdown filters
-  const filteredCards = allCards.filter(card => {
-    const matchesSearchQuery = card.cardName.toLowerCase().includes(searchQuery.toLowerCase());
+  const filteredCards = allCards.filter((card) => {
+    const matchesSearchQuery = card.cardName
+      .toLowerCase()
+      .startsWith(searchQuery.toLowerCase());
     const matchesCardType = cardType ? card.cardType === cardType : true;
-    const matchesCardAttribute = cardAttribute ? card.cardAttribute === cardAttribute : true;
+    const matchesCardAttribute = cardAttribute
+      ? card.cardAttribute === cardAttribute
+      : true;
     const matchesCardClass = cardClass ? card.cardClass === cardClass : true;
-    const matchesCardCharacter = cardCharacter ? card.cardCharacter === cardCharacter : true;
-    const matchesCardLevel = cardLevel ? card.cardLevel === Number(cardLevel) : true;
+    const matchesCardCharacter = cardCharacter
+      ? card.cardCharacter === cardCharacter
+      : true;
+    const matchesCardLevel = cardLevel
+      ? card.cardLevel === Number(cardLevel)
+      : true;
     const matchesOwnership = ownedOnly ? inventory.includes(card.id) : true;
 
-    return matchesSearchQuery && matchesCardType && matchesCardAttribute && matchesCardClass && 
-           matchesCardCharacter && matchesCardLevel && matchesOwnership;
+    return (
+      matchesSearchQuery &&
+      matchesCardType &&
+      matchesCardAttribute &&
+      matchesCardClass &&
+      matchesCardCharacter &&
+      matchesCardLevel &&
+      matchesOwnership
+    );
   });
 
+  // Handle selecting a card
+  const handleCardClick = (card) => {
+    setSelectedCard(card); // Update the selected card in the state
+  };
+
+  // Handle search query input
+  const handleSearch = (event) => {
+    const query = event.target.value.toLowerCase();
+    setSearchQuery(query);
+
+    // Filter inventory based on card name
+    const filtered = inventoryCards.filter((card) =>
+      card.cardName.toLowerCase().startsWith(query)
+    );
+    setFilteredCards(filtered);
+  };
+
   return (
-    <div>
-      <h1>Dictionary</h1>
-      
-      {/* Search Bar */}
-      <div className="search-bar">
-        <input
-          type="text"
-          placeholder="Search cards..."
-          value={searchQuery}
-          onChange={(e) => setSearchQuery(e.target.value)} // Update search query on change
-        />
-      </div>
+    <main
+      id="dictionary"
+      style={{ backgroundImage: `url(${inventoryBg})` }}
+      className="sm:px-12 lg:px-24"
+    >
+      <div className="overlay"></div>
 
-      {/* Checkbox for "Owned Only" filter */}
-      <div className="filter-checkbox">
-        <input
-          type="checkbox"
-          checked={ownedOnly}
-          onChange={() => setOwnedOnly(prev => !prev)}
-        />
-        <label>Owned</label>
-      </div>
+      <div className="wrapper sm:pt-4 lg:pt-12 text-white">
+        <div className="cards">
+          <div className="mb-4 flex justify-between">
+            <h1 className="sm:text-4xl lg:text-6xl">Dictionary</h1>
 
-      {/* Dropdown for Card Type */}
-      <div className="filter-dropdown">
-        <label>Card Type</label>
-        <select value={cardType} onChange={(e) => setCardType(e.target.value)}>
-          <option value="">Select Card Type</option>
-          <option value="monster">Monster</option>
-          <option value="trap">Trap</option>
-          <option value="spell">Spell</option>
-        </select>
-      </div>
-
-      {/* Conditional Dropdowns for "Monster" card type */}
-      {cardType === "monster" && (
-        <>
-          <div className="filter-dropdown">
-            <label>Card Attribute</label>
-            <select value={cardAttribute} onChange={(e) => setCardAttribute(e.target.value)}>
-              <option value="">Select Card Attribute</option>
-              <option value="fire">Fire</option>
-              <option value="water">Water</option>
-              <option value="wind">Wind</option>
-              <option value="earth">Earth</option>
-              <option value="dark">Dark</option>
-              <option value="light">Light</option>
-              <option value="divine">Divine</option>
-            </select>
-          </div>
-
-          <div className="filter-dropdown">
-            <label>Card Class</label>
-            <select value={cardClass} onChange={(e) => setCardClass(e.target.value)}>
-              <option value="">Select Card Class</option>
-              <option value="warrior">warrior</option>
-              <option value="beast">beast</option>
-              <option value="fairy">fairy</option>
-              <option value="spellcaster">spellcaster</option>
-              <option value="serpent">serpent</option>
-              <option value="dragon">dragon</option>
-              <option value="rock">rock</option>
-              <option value="machine">machine</option>
-            </select>
-          </div>
-
-          <div className="filter-dropdown">
-            <label>Card Character</label>
-            <select value={cardCharacter} onChange={(e) => setCardCharacter(e.target.value)}>
-              <option value="">Select Card Character</option>
-              <option value="normal">normal</option>
-              <option value="effect">effect</option>
-              <option value="fusion">fusion</option>
-              <option value="ritual">ritual</option>
-            </select>
-          </div>
-
-          <div className="filter-dropdown">
-            <label>Card Level</label>
             <input
-              type="number"
-              value={cardLevel}
-              onChange={(e) => setCardLevel(e.target.value)}
-              placeholder="Enter card level"
-            />
+              type="text"
+              placeholder="Search cards..."
+              value={searchQuery}
+              onChange={handleSearch}
+              class="px-4 sm:me-5 lg:me-12 bg-transparent border-2 rounded-xl outline-none"
+            ></input>
           </div>
-        </>
-      )}
 
-      <div className="cards-container">
-        {/* Display owned cards first */}
-        {filteredCards.filter(card => inventory.includes(card.id)).map((card) => (
-          <div key={card.id} className="card">
-            <img src={card.imageUrl} alt={card.cardName} />
-            <p>{card.cardName}</p>
-          </div>
-        ))}
+          <div className="flex flex-wrap justify-between sm:me-5 lg:me-12 mb-4 sm:text-xs lg:text-lg">
+            {/* Dropdown for Card Type */}
+            <select
+              value={cardType}
+              onChange={(e) => setCardType(e.target.value)}
+              className="p-2 xl:px-4 bg-transparent border-2 rounded-xl outline-none"
+            >
+              <option value="">Card Type</option>
+              <option value="monster">Monster</option>
+              <option value="trap">Trap</option>
+              <option value="spell">Spell</option>
+            </select>
 
-        {/* Display non-owned cards next */}
-        {filteredCards.filter(card => !inventory.includes(card.id)).map((card) => (
-          <div key={card.id} className={`card ${inventory.includes(card.id) ? "" : "black-and-white"}`}>
-            <img src={card.imageUrl} alt={card.cardName} />
-            <p>{card.cardName}</p>
+            {/* Conditional Dropdowns for "Monster" card type */}
+            {cardType === "monster" && (
+              <>
+                <select
+                  value={cardAttribute}
+                  onChange={(e) => setCardAttribute(e.target.value)}
+                  className="p-2 xl:px-4 bg-transparent border-2 rounded-xl outline-none"
+                >
+                  <option value="">Attribute</option>
+                  <option value="fire">Fire</option>
+                  <option value="water">Water</option>
+                  <option value="wind">Wind</option>
+                  <option value="earth">Earth</option>
+                  <option value="dark">Dark</option>
+                  <option value="light">Light</option>
+                  <option value="divine">Divine</option>
+                </select>
+
+                <select
+                  value={cardClass}
+                  onChange={(e) => setCardClass(e.target.value)}
+                  className="p-2 xl:px-4 bg-transparent border-2 rounded-xl outline-none"
+                >
+                  <option value="">Class</option>
+                  <option value="warrior">Warrior</option>
+                  <option value="beast">Beast</option>
+                  <option value="fairy">Fairy</option>
+                  <option value="spellcaster">Spellcaster</option>
+                  <option value="serpent">Serpent</option>
+                  <option value="dragon">Dragon</option>
+                  <option value="rock">Rock</option>
+                  <option value="machine">Machine</option>
+                </select>
+
+                <select
+                  value={cardCharacter}
+                  onChange={(e) => setCardCharacter(e.target.value)}
+                  className="p-2 xl:px-4 bg-transparent border-2 rounded-xl outline-none"
+                >
+                  <option value="">Character</option>
+                  <option value="normal">Normal</option>
+                  <option value="effect">Effect</option>
+                  <option value="fusion">Fusion</option>
+                  <option value="ritual">Ritual</option>
+                </select>
+
+
+                <input
+                  type="number"
+                  value={cardLevel}
+                  onChange={(e) => setCardLevel(e.target.value)}
+                  placeholder="Card level"
+                  className="p-2 xl:px-4 bg-transparent border-2 rounded-xl outline-none"
+                />
+              </>
+            )}
           </div>
-        ))}
+
+          <div className="collection pb-4">
+            {filteredCards
+              .filter((card) => inventory.includes(card.id))
+              .map((card) => (
+                <img
+                  src={card.imageUrl}
+                  alt={card.cardName || "Card Image"}
+                  onClick={() => handleCardClick(card)}
+                />
+              ))}
+
+            {filteredCards
+              .filter((card) => !inventory.includes(card.id))
+              .map((card) => (
+                <img
+                  className={`${
+                    inventory.includes(card.id) ? "" : "black-and-white"
+                  }`}
+                  src={card.imageUrl}
+                  alt={card.cardName}
+                  onClick={() => handleCardClick(card)}
+                />
+              ))}
+          </div>
+        </div>
+
+        <div className="lastCard sm:ms-2 lg:ms-4">
+          {/* Display the selected card if there is one, otherwise show the placeholder */}
+          {selectedCard ? (
+            <>
+              <img
+                className={`mx-auto w-4/5 sm:my-2 lg:my-4 ${
+                  !inventory.includes(selectedCard.id) ? "black-and-white" : ""
+                }`}
+                src={selectedCard.imageUrl}
+                alt={selectedCard.cardName}
+              />
+
+              <div className="stats sm:text-sm lg:text-2xl flex justify-between sm:w-4/5 lg:w-full mx-auto">
+                <div>
+                  <p>
+                    Matches:{" "}
+                    {selectedCard ? selectedCard.cardMatch?.local || 0 : "N/A"}
+                  </p>
+                  <p>Win Rate: {winRate}</p>
+                </div>
+
+                <div>
+                  <p>Value: {selectedCard?.marketValue || 0}</p>
+                  <p className="text-start">ROI: {formattedROI}</p>
+                </div>
+              </div>
+
+              <div className="flex justify-end sm:h-8 lg:h-10 sm:text-2xl lg:text-4xl mt-6 sm:w-4/5 lg:w-full mx-auto">
+                <img className="me-2" src={gold} alt="gold coins" />
+                <p>{goldCount !== 0 ? goldCount : 0}</p>
+              </div>
+            </>
+          ) : (
+            <p className="text-2xl text-center">Please select a card</p>
+          )}
+        </div>
       </div>
-    </div>
+    </main>
   );
 }
 
 export default DictionaryPage;
- 
