e5550408fc6770b5143232dbedeff066d1afe7fd
MannLester <127029268+MannLester@users.noreply.github.com>
2024-11-24 07:42:57 -0800

    Update Showcase.jsx

--- src/components/Showcase.jsx
+++ src/components/Showcase.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import { useParams, useNavigate } from "react-router-dom";
 import { storage, firestore } from "./firebaseConfig";
 import { ref as storageRef, getDownloadURL } from "firebase/storage";
