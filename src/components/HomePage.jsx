62d027b7678efba3066656a672812f53134e4751
jettmanalo <164733601+jettmanalo@users.noreply.github.com>
2024-11-25 21:27:22 +0800

    Ready for testing

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -1,50 +1,60 @@
-import React from 'react';
-import { Link, useParams } from 'react-router-dom';
-import accountImage from '../assets/images/account.png';
-import battleImage from '../assets/images/battle.png';
-import shopImage from '../assets/images/shop.png';
-import inventoryImage from '../assets/images/inventory.png';
-import dictionaryImage from '../assets/images/dictionary.png';
-import workshopImage from '../assets/images/workshop.png';
-import friendsImage from '../assets/images/friends.png';
-import loginBackground from '../assets/backgrounds/login.jpg';
-import './HomePage.css'
+import React from "react";
+import { Link, useParams } from "react-router-dom";
+import accountImage from "../assets/images/account.png";
+import battleImage from "../assets/images/battle.png";
+import shopImage from "../assets/images/shop.png";
+import inventoryImage from "../assets/images/inventory.png";
+import dictionaryImage from "../assets/images/dictionary.png";
+import workshopImage from "../assets/images/workshop.png";
+import friendsImage from "../assets/images/friends.png";
+import loginBackground from "../assets/backgrounds/login.jpg";
+import "./HomePage.css";
 
 function HomePage() {
   const { userDocId } = useParams();
   return (
-    <main id='home' className='text-center' style={{ backgroundImage: `url(${loginBackground})` }}>
+    <main
+      id="home"
+      className="text-center"
+      style={{ backgroundImage: `url(${loginBackground})` }}
+    >
       <div className="overlay"></div>
 
-      <div className='banners'>
-        <Link to={`/${userDocId}/workshop`}><img src={workshopImage} alt="" /></Link>
-        <Link to={`/${userDocId}/battlefield`}><img src={battleImage} alt="" /></Link>
-        <Link to={`/${userDocId}/shop`}><img src={shopImage} alt="" /></Link>
+      <div className="banners">
+        <Link to={`/${userDocId}/workshop`}>
+          <img src={workshopImage} alt="" />
+        </Link>
+        <Link to={`/${userDocId}/battlefield`}>
+          <img src={battleImage} alt="" />
+        </Link>
+        <Link to={`/${userDocId}/shop`}>
+          <img src={shopImage} alt="" />
+        </Link>
       </div>
 
-      <div className='links text-white text-xl'>
-        <div className='sm:mx-6 lg:mx-12'>
+      <div className="links text-white text-xl">
+        <div className="sm:mx-6 lg:mx-12">
           <Link to={`/${userDocId}/inventory`}>
             <img src={inventoryImage} alt="" />
             <p>Inventory</p>
           </Link>
         </div>
 
-        <div className='sm:mx-6 lg:mx-12'>
+        <div className="sm:mx-6 lg:mx-12">
           <Link to={`/${userDocId}/dictionary`}>
             <img src={dictionaryImage} alt="" />
             <p>Dictionary</p>
           </Link>
         </div>
 
-        <div className='sm:mx-6 lg:mx-12'>
+        <div className="sm:mx-6 lg:mx-12">
           <Link to={`/${userDocId}/friends`}>
             <img src={friendsImage} alt="" />
             <p>Friends</p>
           </Link>
         </div>
 
-        <div className='sm:mx-6 lg:mx-12'>
+        <div className="sm:mx-6 lg:mx-12">
           <Link to={`/${userDocId}/account`}>
             <img src={accountImage} alt="" />
             <p>Account</p>
