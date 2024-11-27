12d522d36ea647d17dae74511ecba1c9bb8aa886
MannLester <127029268+MannLester@users.noreply.github.com>
2024-11-27 06:04:08 -0800

    November 27 Update: Friend Page

---
+++ src/components/mail/FriendRequestMail.jsx
@@ -0,0 +1,99 @@
+import PropTypes from 'prop-types';
+
+import { useState, useEffect } from 'react';
+
+import { doc, getDoc } from 'firebase/firestore';
+
+import { firestore } from '../firebaseConfig';
+
+import './MailStyles.css';
+
+
+
+const FriendRequestMail = ({ mailSender, mailSent, onAccept, onReject }) => {
+
+    const [senderUsername, setSenderUsername] = useState('');
+
+
+
+    useEffect(() => {
+
+        const fetchSenderUsername = async () => {
+
+            try {
+
+                const senderDoc = await getDoc(doc(firestore, 'users', mailSender));
+
+                if (senderDoc.exists()) {
+
+                    setSenderUsername(senderDoc.data().username);
+
+                }
+
+            } catch (error) {
+
+                console.error("Error fetching sender username:", error);
+
+            }
+
+        };
+
+
+
+        fetchSenderUsername();
+
+    }, [mailSender]);
+
+
+
+    return (
+
+        <div className="mail-item friend-request-mail">
+
+            <div className="mail-content-wrapper">
+
+                <div className="mail-text">
+
+                    {senderUsername} has sent you a friend request!
+
+                </div>
+
+                <div className="mail-info">
+
+                    <span>Sent: {mailSent.toDate().toLocaleString()}</span>
+
+                </div>
+
+            </div>
+
+            <div className="action-buttons">
+
+                <button className="accept-button" onClick={onAccept}>✓</button>
+
+                <button className="reject-button" onClick={onReject}>✕</button>
+
+            </div>
+
+        </div>
+
+    );
+
+};
+
+
+
+FriendRequestMail.propTypes = {
+
+    mailSender: PropTypes.string.isRequired,
+
+    mailSent: PropTypes.object.isRequired,
+
+    onAccept: PropTypes.func.isRequired,
+
+    onReject: PropTypes.func.isRequired
+
+};
+
+
+
+export default FriendRequestMail; 
